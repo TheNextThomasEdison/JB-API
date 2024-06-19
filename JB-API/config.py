0b6874c1458469f28142fd311e305701522ab2f7
TheNextThomasEdison <daisypraiseebireri@gmail.com>
2024-06-19 23:22:36 +0100

    Attempting to finalize server-side app

--- JB-API/config.py
+++ JB-API/config.py
@@ -10,10 +10,12 @@ load_dotenv('sens_info.env')
 db_user = os.environ["DB_PASSWORD"]
 db_password = os.environ["DB_USERNAME"]
 db_name = os.environ["DB_NAME"]
+db_host = os.environ["DB_HOST"]
 
 # Create a MySQL connection
 app.config['MYSQL_USER'] = db_user
 app.config['MYSQL_PASSWORD'] = db_password
+app.config['MYSQL_HOST'] = db_host
 app.config['MYSQL_DB'] = db_name
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 _mysql = MySQL(app)
